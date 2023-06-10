c15d3ab2d2f5317b7aba60f57d76a87fb6d9660c
inviolable <inviolable@naver.com>
2023-06-10 18:50:37 +0900

    Add files via upload

---
+++ ROOT/src/main/java/com/example/demo/BootPractice01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPractice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPractice01Application.class, args);
+	}
+
+}
---
+++ ROOT/src/main/webapp/WEB_INF/jsp/main.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>메인 페이지</title>
+</head>
+<body>
+안녕하세요, 메인 페이지입니다.
+</body>
+</html>
